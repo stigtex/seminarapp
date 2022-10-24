d96ddb9c2e12d47bb58dfd24b35aa28fc457d087
Stig <stig@texdahl.com>
2022-10-24 19:11:13 +0200

    Program

---
+++ SeminarApplication/Program.cs
@@ -0,0 +1,47 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using SeminarApplication.Contracts.Responses;
+using SeminarApplication.Database;
+using SeminarApplication.Repositories;
+using SeminarApplication.Services;
+using SeminarApplication.Validation;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddFastEndpoints();
+builder.Services.AddSwaggerDoc();
+
+builder.Services.AddSingleton<ISqlConnectionFactory>(_ =>
+    new SqlConnectionFactory(config.GetValue<string>("Database:ConnectionString")!));
+builder.Services.AddSingleton<Initializer>();
+
+builder.Services.AddSingleton<IParticipantRepository, ParticipantRepository>();
+builder.Services.AddSingleton<ICourseRepository, CourseRepository>();
+builder.Services.AddSingleton<ISeminarRepository, SeminarRepository>();
+
+builder.Services.AddSingleton<IParticipantService, ParticipantService>();
+builder.Services.AddSingleton<ICourseService, CourseService>();
+builder.Services.AddSingleton<ISeminarService, SeminarService>();
+
+var app = builder.Build();
+
+app.UseOpenApi();
+app.UseSwaggerUi3(s => s.ConfigureDefaults());
+
+app.UseMiddleware<ValidationExceptionMiddleware>();
+app.UseFastEndpoints(x =>
+{
+    x.ErrorResponseBuilder = (failures, _) =>
+    {
+        return new ValidationFailureResponse
+        {
+            Errors = failures.Select(y => y.ErrorMessage).ToList()
+        };
+    };
+});
+
+var databaseInitializer = app.Services.GetRequiredService<Initializer>();
+await databaseInitializer.InitializeAsync();
+
+app.Run();
